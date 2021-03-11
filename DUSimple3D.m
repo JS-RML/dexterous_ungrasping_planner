06d369dd545781b1c5006eeb7deed007ba2650b3
ansonmak <ansonmak1997@gmail.com>
2021-03-11 20:29:20 +0800

    Minor fix after merging

--- DUSimple3D.m
+++ DUSimple3D.m
@@ -24,6 +24,7 @@ classdef DUSimple3D < handle
         list
         num_rewired
         mode_change_weight
+        goal_bias
         % boolen for enabling finger's collision check
         finger_collision_check
     end
@@ -55,6 +56,7 @@ classdef DUSimple3D < handle
             this.list = 1:max_nodes;
             this.num_rewired = 0;
             this.mode_change_weight = conf.mode_change_weight;
+            this.goal_bias = conf.goal_bias;
             %finger's collision check
             this.finger_collision_check = false; %TODO: change to true if enable finger collision check
         end
@@ -67,7 +69,7 @@ classdef DUSimple3D < handle
             %    + [this.XYZ_BOUNDARY(1);this.XYZ_BOUNDARY(3); this.start_point(3)];
             %position = [this.XYZ_BOUNDARY(2) - this.XYZ_BOUNDARY(1); this.XYZ_BOUNDARY(4) - this.XYZ_BOUNDARY(3); this.XYZ_BOUNDARY(6) - this.XYZ_BOUNDARY(5)] .* rand(3,1) ...
             %    + [this.XYZ_BOUNDARY(1);this.XYZ_BOUNDARY(3); this.XYZ_BOUNDARY(5)];
-            if rand < 0.5
+            if rand < this.goal_bias
                 position = this.goal_point';
             end
         end
@@ -82,19 +84,32 @@ classdef DUSimple3D < handle
         end
         
         function position = steer(this, nearest_node, new_node_position)
-            sample_rate = 5;
+            sample_rate = 2; %numbers of sample of the slope of diagonal path, 2: fixed slope
             if nearest_node == 1
                 parent_of_nearest_node = 1;
             else
                 parent_of_nearest_node = this.parent(nearest_node);
             end
             mode_of_prev = this.contact_mode(this.tree(:, nearest_node)-this.tree(:, parent_of_nearest_node));
-            candidates = this.feasbile_action(this.max_step, this.tree(:, nearest_node), sample_rate);
+            if sample_rate == 2
+                angle = atand(this.goal_point(2)/((this.start_point(3)-this.goal_point(3))*100));
+                candidates = this.feasbile_actiond(this.max_step, this.tree(:, nearest_node), angle);
+            else
+                candidates = this.feasbile_action(this.max_step, this.tree(:, nearest_node), sample_rate);
+            end
             mode_of_candidates = this.contact_mode(candidates-repmat(this.tree(:, nearest_node),1,size(candidates,2)));
             mode_change_cost =  abs(mode_of_candidates-repmat(mode_of_prev,1,size(mode_of_candidates,2)));
             costs = this.cost_function(repmat(new_node_position, 1, sample_rate+1), candidates) + this.mode_change_weight*mode_change_cost;
             [m,i] = min(costs);
             position = candidates(:,i);
+            %{
+            if position(3) < this.goal_point(3)
+                position(3) = this.goal_point(3);
+            end
+            if position(2) > this.goal_point(2)
+                position(2) = this.goal_point(2);
+            end
+            %}
             %{
             relative_position = new_node_position - this.tree(:, nearest_node); 
             dist = this.cost_function(relative_position, [0; 0; 0]); 
@@ -158,7 +173,7 @@ classdef DUSimple3D < handle
             this.cost(this.nodes_added) = this.cost_function(this.tree(:, parent_node_ind), new_node_position);  % not that important
             % TODO: Add a line to take into account change of contact
             if parent_node_ind==1
-                mode_of_prev = 2;
+                mode_of_prev = 3; %2
             else
                 mode_of_prev = this.contact_mode(this.tree(:, parent_node_ind)-this.tree(:, this.parent(parent_node_ind)));
                 %[this.tree(:, parent_node_ind)-this.tree(:, this.parent(parent_node_ind))]
@@ -271,7 +286,7 @@ classdef DUSimple3D < handle
             format short;
             flipud([round(backtrace_path',0) round(this.cumcost(backtrace_path)',2) round(this.tree(:,backtrace_path)',2)])
             
-            draw_nodes = 0;
+            draw_nodes = 1;
             if draw_nodes==1
                 drawn_nodes = zeros(1, this.nodes_added);
                 for ind = this.nodes_added:-1:1;
@@ -292,10 +307,12 @@ classdef DUSimple3D < handle
                     end
                 end
             end
-            plot3(this.tree(1,backtrace_path), this.tree(2,backtrace_path), this.tree(3,backtrace_path), '*b-','LineWidth', 2);
-            plot3(this.tree(1, 1), this.tree(2, 1), this.tree(3, 1), '-o','Color','r','MarkerSize',10,'MarkerFaceColor','r')
-            plot3(this.goal_point(1), this.goal_point(2), this.goal_point(3),'-o','Color','m','MarkerSize',10,'MarkerFaceColor','m')
-            
+            plot3(this.tree(1,backtrace_path), this.tree(2,backtrace_path), this.tree(3,backtrace_path), '-.k','LineWidth', 2.5);
+            %plot3(this.tree(1, 1), this.tree(2, 1), this.tree(3, 1), '-o','Color','r','MarkerSize',10,'MarkerFaceColor','r')
+            %plot3(this.goal_point(1), this.goal_point(2), this.goal_point(3),'-o','Color','m','MarkerSize',10,'MarkerFaceColor','m')
+            plot3(this.tree(1, 1), this.tree(2, 1), this.tree(3, 1), '-o','Color','#0072BD','MarkerSize',10,'MarkerFaceColor','#0072BD')
+            plot3(this.goal_point(1), this.goal_point(2), this.goal_point(3),'-o','Color','#77AC30','MarkerSize',10,'MarkerFaceColor','#77AC30')
+
             %Save output path as csv file
             output_path = 0;
             if output_path == 1
@@ -389,9 +406,16 @@ classdef DUSimple3D < handle
                 candidates(:,i+1) = parent_node_pos + [0 ; max_step*sind(x*(i-1)) ; -(max_step/100)*cosd(x*(i-1))];
             end
         end
+
+        function candidates = feasbile_actiond(max_step, parent_node_pos, angle)
+            candidates = zeros(3, 3);
+            candidates(:, 1) = parent_node_pos + [-max_step;0;0];
+            candidates(:, 2) = parent_node_pos + [0;max_step;0];
+            candidates(:, 3) = parent_node_pos + [0;max_step*sind(angle);-(max_step/100)*cosd(angle)];
+        end 
         
         % Monotonic actions only
-        function candidates = feasbile_actiond(max_step, parent_node_pos, sample_rate)
+        function candidates = feasbile_action_mono(max_step, parent_node_pos, sample_rate)
             candidates = zeros(3, sample_rate+1);
             candidates(:, 1) = parent_node_pos + [-max_step;0;0];
             candidates(:, 2) = parent_node_pos + [0;max_step;0];
--- main_DUSimple3D_rrt_star.m
+++ main_DUSimple3D_rrt_star.m
@@ -1,6 +1,6 @@
 
 clc;clear;
-map = struct('name', 'my_map.mat', 'start_point', [40 0 0.5], 'goal_point', [0 20 0.2]);
+map = struct('name', 'my_map.mat', 'start_point', [30 0 0.5], 'goal_point', [0 25 0.3]);
 max_iter = 1e3;
 is_benchmark = false;
 rand_seed = 40;
